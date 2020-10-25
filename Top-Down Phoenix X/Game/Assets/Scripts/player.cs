bc8c8d5fb56cadb902dde289ea9561788fbe673d
Murray Moore <51520159+rrumrum@users.noreply.github.com>
2020-10-25 15:00:09 -0400

    beginnings of movement

---
+++ Top-Down Phoenix X/Game/Assets/Scripts/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+
+public class player : MonoBehaviour
+{
+    public float speed;
+    public float slowMutliplier;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+
+        Vector3 motion = movementInput;
+        motion *= speed;
+        motion *= (Input.GetButton("slow")) ? slowMutliplier : 1;
+
+        controller.Move(motion * Time.deltaTime);
+    }
+}
