66ebb94e69c02e7e624ddfe6ca76c2e37e3c5c91
Vu Nguyen <45470746+Lominoe@users.noreply.github.com>
2020-11-17 22:26:18 -0500

    New project file

---
+++ Mostly Full IB Top Ten/Assets/Scripts/AwarenessScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AwarenessScript : MonoBehaviour
+{
+    public bool enemiesInRange;
+    Vector3 vel = Vector3.zero;
+    public void DetectInteractables(float range, LayerMask interactables, int maxTargets, Transform look)
+    {
+        //Get any colliders in this range that are on the layermask
+        Collider[] interactablesInRange = Physics.OverlapSphere(transform.position, range, interactables);
+
+        //For if more than maxTargets were found
+        if (interactablesInRange.Length > maxTargets)
+        {
+            //sort by distance
+            interactablesInRange.OrderBy(hit => Vector3.Distance(hit.transform.position, transform.position));
+        }
+
+        //This is a list for all of the interactables in the range
+        List<GameObject> interactablesToHit = new List<GameObject>();
+
+        //populate the tnteractablesToHit list
+        for (int i = 0; i < maxTargets; i++)
+        {
+            if (i < interactablesInRange.Length)
+            {
+                interactablesToHit.Add(interactablesInRange[i].gameObject);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        foreach (GameObject interactable in interactablesToHit)
+        {
+            print(interactablesToHit.Count + " interactables");
+            look.position = Vector3.SmoothDamp(look.position,interactablesToHit[0].transform.position, ref vel, 100f * Time.deltaTime);
+        }
+    }
+
+    public void DetectEnemies(float range, LayerMask enemies, int maxTargets)
+    {
+        //Get any colliders in this range that are on the layermask
+        Collider[] EnemiesInRange = Physics.OverlapSphere(transform.position, range, enemies, QueryTriggerInteraction.Collide);
+
+        //For if more than maxTargets were found
+        if (EnemiesInRange.Length > maxTargets)
+        {
+            //sort by distance
+            EnemiesInRange.OrderBy(hit => Vector3.Distance(hit.transform.position, transform.position));
+        }
+
+        //This is a list for all of the interactables in the range
+        List<GameObject> enemiesToHit = new List<GameObject>();
+
+        //populate the tnteractablesToHit list
+        for (int i = 0; i < maxTargets; i++)
+        {
+            if (i < EnemiesInRange.Length)
+            {
+                enemiesToHit.Add(EnemiesInRange[i].gameObject);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (enemiesToHit.Count > 0)
+        {
+            enemiesInRange = true;
+        }
+        else if (enemiesToHit.Count == 0)
+        {
+            StartCoroutine(EndFight(5));
+        }
+        foreach (GameObject enemy in enemiesToHit)
+        {
+            print(enemiesToHit.Count + " Enemies");
+        }
+    }
+    IEnumerator EndFight(float wait)
+    {
+        yield return new WaitForSeconds(wait);
+        enemiesInRange = false;
+    }
+}
---
+++ Mostly Full IB Top Ten/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    AwarenessScript eyes;
+    public LayerMask interactable;
+    int maxTargets = 3;
+    float range = 20f;
+    public Transform lookTarget;
+    public Transform followTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        eyes = GetComponent<AwarenessScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        eyes.DetectInteractables(range, interactable, maxTargets, lookTarget);
+    }
+}
